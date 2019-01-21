77570efcaae9ac0b03b0d7c1f90d1cdd779f152a
stevojed <jedi1014@comcast.net>
2019-01-21 14:32:06 -0500

    Added Special Character Check

--- Week 1 Capstone/Program.cs
+++ Week 1 Capstone/Program.cs
@@ -10,18 +10,22 @@ namespace Week_1_Capstone
     {
         static void Main(string[] args)
         {
+            string input= "";
+            char[] vowel = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            string[] punc = { "!", "@", "#", "$", "%", "^", "&", "*", ",", "." };
             bool again = true;
             while (again == true)
             {
-                Console.WriteLine("Welcome to the Pig Latin Translator!");
-                Console.WriteLine("Enter a word to be translated");
+                Console.WriteLine("Welcome to the Pig Latin Translator!\n");
+                Console.Write("Enter a sentence to be translated: ");
+                
+                    input = Console.ReadLine();
+                    int length = input.Length;
+                    string[] results = input.Split(' ');
+                    int res = results.Length - 1;
 
-                string input = Console.ReadLine();
-                int length = input.Length;
-                string[] results = input.Split(' ');
-                int res = results.Length - 1;
-
-                char[] vowel = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+                Console.Write("\nYour translated words are: ");
+                
                 foreach (string s in results)
                 {
                     char[] q = s.ToArray();
@@ -30,10 +34,17 @@ namespace Week_1_Capstone
                     string rem = s.Substring(x, q.Length - x);
                     string combo = rem + p;
                     string[] combined = combo.Split(' ');
-
+                    
                     for (int n = 0; n < combined.Length; n++)
                     {
-                        if (s.StartsWith("a") || s.StartsWith("e") || s.StartsWith("i") || s.StartsWith("o") || s.StartsWith("u") ||
+                        if (s.Contains("!") || s.Contains("@") || s.Contains("#") || s.Contains("$")
+                            || s.Contains("%") || s.Contains("^") || s.Contains("&") || s.Contains("*")
+                            || s.Contains("+") || s.Contains(",") || s.Contains("."))
+                        {
+                            int k = n;
+                            combined[k] = combined[k].Replace(combined[k], s);
+                        }
+                        else if (s.StartsWith("a") || s.StartsWith("e") || s.StartsWith("i") || s.StartsWith("o") || s.StartsWith("u") ||
                             s.StartsWith("A") || s.StartsWith("E") || s.StartsWith("I") || s.StartsWith("O") || s.StartsWith("U"))
                         {
                             int m = n;
@@ -43,20 +54,36 @@ namespace Week_1_Capstone
                         {
                             int l = n;
                             combined[l] = combined[l].Replace(combined[l], combined[l] + "ay");
-                        }
+                        }                        
                         Console.Write(combined[n] + " ");
                     }
                 }
-                Console.Write("\nTranslate another line? (y/n): ");
+                Console.Write("\n\nTranslate another line? (y/n): ");
                 string rep = Console.ReadLine().ToLower();
                 if (rep != "y")
+                {                    
+                    again = false;
+                }
+                else
                 {
                     Console.Clear();
-                    again = false;
                 }
             }
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
+        }
+        //       NOT WORKING
+        public static string [] Punc(string[] punct)
+        {
+            string input = "";
+            char[] punc = { '!', '@', '#', '$', '%', '^', '&', '*', ',', '.' };
+
+            for (int i = 0; i < punc.Length; i++)
+            {
+                input.Contains(punc[i]);                
+            }
+            return punct;
+        //      NOT WORKING
         }
     }
 }
